249d1090f068bd689c6aab1286a5c969b17947db
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-03-17 11:42:55 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
 radio.set_group(69)
 pocatek=False
+ukonceni=False
 konec=0
 zacatek=0
-RunComp.set_light_level()
+cas=0
+
 def on_button_pressed_a():
     global pocatek
     pocatek=True
+    basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    RunComp.set_light_level() 
+    music.play_tone(Note.C, music.beat(500))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_event_pressed():
+    global pocatek, ukonceni, konec, zacatek, cas
+    radio.send_number(3)
+    pocatek=False
+    ukonceni=False
+    konec=0
+    zacatek=0
+    cas=0
+    basic.clear_screen()
+    music.play_tone(Note.C, music.beat(500))
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
 def on_light_drop():
-    global pocatek
+    global pocatek, ukonceni
     if pocatek==True:
         radio.send_number(1)
         pocatek=False
+        music.play_tone(Note.D, music.beat(500))
+    elif ukonceni==True:
+        global konec, cas, zacatek, ukonceni
+        ukonceni=False
+        konec=control.millis()
+        cas=konec-zacatek
+        music.play_tone(Note.C, music.beat(500))
+        while pocatek==False:
+            basic.show_number(cas/1000)
 RunComp.on_light_drop(on_light_drop)
 
 def on_received_number(receivedNumber):
-    zacatek=control.millis()
-    def on_light_drop2():
-        konec=control.millis()
-        cas=konec-zacatek
-        console.log_value("recieved", receivedNumber)
-        console.log_value("konec", konec)
-        console.log_value("cas", cas)
-        while pocatek==False:
-            basic.show_number(cas/1000)
-    RunComp.on_light_drop(on_light_drop2)
+
+    global zacatek, ukonceni, konec, cas, pocatek
+    if receivedNumber==1:
+        ukonceni=True
+        zacatek=control.millis()
+        music.play_tone(Note.C, music.beat(500))
+       
+    elif receivedNumber==3:
+        pocatek=False
+        ukonceni=False
+        konec=0
+        zacatek=0
+        cas=0
+        basic.clear_screen()
+        music.play_tone(Note.C, music.beat(500))
 radio.on_received_number(on_received_number)
 
-def onIn_background():
-    def on_forever():
-        console.log_value("pocatek", pocatek)
-    basic.forever(on_forever)
-control.in_background(onIn_background)
+def on_forever():
+    console.log_value("pocatek", pocatek)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,35 +1,64 @@
 radio.setGroup(69)
 let pocatek = false
+let ukonceni = false
 let konec = 0
 let zacatek = 0
-RunComp.SetLightLevel()
+let cas = 0
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     pocatek = true
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    RunComp.SetLightLevel()
+    music.playTone(Note.C, music.beat(500))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    
+    radio.sendNumber(3)
+    pocatek = false
+    ukonceni = false
+    konec = 0
+    zacatek = 0
+    cas = 0
+    basic.clearScreen()
+    music.playTone(Note.C, music.beat(500))
 })
 RunComp.OnLightDrop(function on_light_drop() {
     
     if (pocatek == true) {
         radio.sendNumber(1)
         pocatek = false
+        music.playTone(Note.D, music.beat(500))
+    } else if (ukonceni == true) {
+        
+        ukonceni = false
+        konec = control.millis()
+        cas = konec - zacatek
+        music.playTone(Note.C, music.beat(500))
+        while (pocatek == false) {
+            basic.showNumber(cas / 1000)
+        }
     }
     
 })
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    let zacatek = control.millis()
-    RunComp.OnLightDrop(function on_light_drop2() {
-        let konec = control.millis()
-        let cas = konec - zacatek
-        console.logValue("recieved", receivedNumber)
-        console.logValue("konec", konec)
-        console.logValue("cas", cas)
-        while (pocatek == false) {
-            basic.showNumber(cas / 1000)
-        }
-    })
+    
+    if (receivedNumber == 1) {
+        ukonceni = true
+        zacatek = control.millis()
+        music.playTone(Note.C, music.beat(500))
+    } else if (receivedNumber == 3) {
+        pocatek = false
+        ukonceni = false
+        konec = 0
+        zacatek = 0
+        cas = 0
+        basic.clearScreen()
+        music.playTone(Note.C, music.beat(500))
+    }
+    
 })
-control.inBackground(function onIn_background() {
-    basic.forever(function on_forever() {
-        console.logValue("pocatek", pocatek)
-    })
+basic.forever(function on_forever() {
+    console.logValue("pocatek", pocatek)
 })
